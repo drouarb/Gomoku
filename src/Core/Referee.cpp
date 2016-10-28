b42f3071b09730ab869579a487eadd4f657a564b
Alexis Trouve <alexis.trouve@epitech.eu>
2016-10-28 06:20:45 -0700

    referee.cpp methods created

---
+++ src/Core/Referee.cpp
@@ -0,0 +1,38 @@
+#include "Referee.hh"
+
+Core::Referee::Referee()
+{
+}
+
+Core::Referee::~Referee()
+{
+}
+
+GameBoard_t	getBoardCopy()
+{
+}
+
+const GameBoard_t&	Core::referee::getBoardRef()
+{
+
+}
+
+void		Core::referee::setPlayer(TEAM player)
+{
+  
+}
+
+bool		Core::referee::tryPlay(uint8_t x, uint8_t y)
+{
+
+}
+
+bool		Core::referee::tryPlay(uint8_t x, uint8_t y)
+{
+
+}
+
+const BoardOperator&	Core::referee::getBoardOperator()
+{
+
+}
