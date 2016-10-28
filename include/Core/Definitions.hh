2a1ece61cb4614beb8741ac0018f0ce2f23d11f3
lewis_e <esteban.lewis@epitech.eu>
2016-10-28 14:57:47 +0200

    core definitions

---
+++ include/Core/Definitions.hh
@@ -0,0 +1,24 @@
+#ifndef CORE_DEFINITIONS_HH_
+#define CORE_DEFINITIONS_HH_
+
+#include <stddef.h>
+
+namespace Core
+{
+    enum GamePlayers : uint8_t
+    {
+        ONEPLAYER,
+        TWOPLAYERS,
+        TWOAIS
+    };
+
+    enum Team : uint8_t
+    {
+        WHITE,
+        BLACK
+    };
+
+    typedef char GameBoard_t[19 * 19];
+}
+
+#endif
