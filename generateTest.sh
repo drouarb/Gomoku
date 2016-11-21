55145d923cc758ced37471f407a5e8f759df3fd5
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-11-21 23:36:03 +0100

    fix log compile

--- generateTest.sh
+++ generateTest.sh
@@ -18,15 +18,15 @@ do
     echo "//Auto Generated tests
 
 #include <Core/Core.hh>
-#include <fstream>
+//#include <fstream>
 
 
 int main() {
 
     Core::Core *core;
 
-    std::ofstream out(\"$file.res\");
-    std::cout.rdbuf(out.rdbuf());
+  //  std::ofstream out(\"$file.res\");
+  //  std::cout.rdbuf(out.rdbuf());
 
 
     try {
