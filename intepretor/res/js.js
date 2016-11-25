c1fbbc793ad0dfe4dc4927c34482acd0e9c06c16
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-11-25 15:25:05 +0100

    change line style

--- intepretor/res/js.js
+++ intepretor/res/js.js
@@ -134,8 +134,9 @@ function fillPattern(ctx) {
         }
         ctx.moveTo(res[0].x * (710 /19) - 3, res[0].y * (710 /19) - 3);
         ctx.lineTo(res[res.length - 1].x * (710 /19) - 3, res[res.length - 1].y * (710 /19) - 3);
+        ctx.stroke();
         ctx.lineWidth = 3;
-        //ctx.strokeStyle = '#ff0000';
+        ctx.strokeStyle = '#ff0000';
         ctx.stroke();
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#000000';
