04850b30d962895a9596dfde2159d14dd1c420f9
Grégoire Guemas <gregoire.guemas@epitech.ey>
2016-11-25 10:37:08 +0100

    Adding values.json BoardEvaluator =>    Adding getInstance() method //explicit    Adding getValue() method // get value of board
       //To test

--- include/Core/BoardEvaluator.hh
+++ include/Core/BoardEvaluator.hh
@@ -5,21 +5,53 @@
 #ifndef GOMOKU_BOARDEVALUATOR_HH
 #define GOMOKU_BOARDEVALUATOR_HH
 
+#include <cstring>
+#include "Referee.hpp"
 #include "PatternManager.hh"
 
 namespace Core {
 
+
+
     class BoardEvaluator {
 
     private:
-        typedef int (*fct_t)(Core::Pattern *);
 
-/*
-        static fct_t fct = NULL;
-        */
-        static int defaultValue(Pattern *p);
+        struct value_t {
+            int size;
+            int extremity[3];
+
+            value_t() {
+                memset(this, 0, sizeof(*this));
+            }
+        };
+
+        struct exceeded_values_t {
+            int points_par_pierre_en_plus;
+            int points_en_plus_en_fonction_des_extremites[3];
+        };
+
+        struct conf_t {
+            value_t values[21];
+            exceeded_values_t exceeded_values;
+            int max_values;
+
+            int points_par_pierre_mangee;
+
+            conf_t() {
+                memset(this, 0, sizeof(*this));
+            }
+        };
+
+
+        conf_t *conf;
+
     public:
-        static inline int getValue(const PatternManager *patternManager);
+        void parseFrom(const std::string &path);
+        inline int32_t getValue(const PatternManager *patternManager, IReferee *referee) const;
+        inline int32_t getValue(const PatternManager *patternManager, IReferee *referee, Team t) const;
+
+        static inline BoardEvaluator *getInstance();
     };
 
 
--- src/Core/BoardEvaluator.cpp
+++ src/Core/BoardEvaluator.cpp
@@ -2,57 +2,87 @@
 // Created by greg on 13/11/2016.
 //
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
 #include "BoardEvaluator.hh"
 
 using namespace Core;
+using namespace boost::property_tree;
 
+static BoardEvaluator *instance = NULL;
 
-
-/*
-const Core::BoardEvaluator::fct_t *Core::BoardEvaluator::fct = {
-    t,
-};
-*/
-
-int Core::BoardEvaluator::getValue(const Core::PatternManager *patternManager) {
-    const PLIST<Pattern> &list = patternManager->getPatterns();
-    int boardValue = 0;
-    static int (*fct[])(Core::Pattern *) = {
-            [](Pattern *p) { //0
-                return 1;
-            },
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-            defaultValue,
-    };
-
-    for (auto e : patternManager->getPatterns()) {
-        if (e.lineLength > 20) {
-            boardValue += defaultValue(&e);
-            continue;
-        }
-        boardValue += fct[e.lineLength](&e);
+BoardEvaluator *BoardEvaluator::getInstance() {
+    if (!instance) {
+        instance = new BoardEvaluator();
     }
-    return boardValue;
+    return instance;
 }
 
-int BoardEvaluator::defaultValue(Pattern *p) {
+int32_t BoardEvaluator::getValue(const PatternManager *patternManager, IReferee *referee) const {
+    ;
     return 0;
 }
+
+int32_t BoardEvaluator::getValue(const PatternManager *patternManager, IReferee *referee, Team t) const {
+    int32_t totalValue = referee->getTeamEat(t) * this->conf->points_par_pierre_mangee;
+    for (auto pattern : patternManager->getPatterns()) {
+        if (pattern.lineLength > this->conf->max_values) {
+            int exceed_rock = pattern.lineLength - this->conf->max_values;
+            totalValue += this->conf->exceeded_values.points_par_pierre_en_plus * exceed_rock;
+            uint8_t freeExtremity = 0;
+            freeExtremity += (pattern.line[0] == 0 ? 1 : 0);
+            freeExtremity += (pattern.lineLength > 0 && pattern.line[pattern.lineLength - 1] == 0 ? 1 : 0);
+            totalValue += this->conf->exceeded_values.points_en_plus_en_fonction_des_extremites[freeExtremity];
+            continue;
+        }
+        value_t &value = this->conf->values[pattern.lineLength];
+        uint8_t freeExtremity = 0;
+        freeExtremity += (pattern.line[0] == 0 ? 1 : 0);
+        freeExtremity += (pattern.lineLength > 0 && pattern.line[pattern.lineLength - 1] == 0 ? 1 : 0);
+        totalValue += value.extremity[freeExtremity];
+    }
+    return totalValue;
+}
+
+
+void BoardEvaluator::parseFrom(const std::string &path) {
+    // Create a root
+    ptree root;
+
+    // Load the json file in this ptree
+    read_json(path, root);
+
+    // BOOST_FOREACH(boost::property_tree::ptree::value_type &v, root.get_child("values")) {
+    //   std::cout << v.first << std::endl;
+    // }
+
+    BOOST_FOREACH(ptree::value_type
+                          child, root.get_child("values")) {
+
+                    int len = child.second.get<int>("taille");
+
+                    value_t *val = &conf->values[len];
+
+                    this->conf->max_values = (this->conf->max_values < len ? len : this->conf->max_values);
+
+                    val->size = len;
+                    uint8_t c = 0;
+
+                    BOOST_FOREACH(const ptree::value_type &child2,
+                                  child.second.get_child("extremites")) {
+                                    val->extremity[c] = child2.second.get_value<int>();
+                                    c++;
+                                }
+                }
+    uint8_t c = 0;
+    conf->exceeded_values.points_par_pierre_en_plus = root.get_child(
+            "exceeded_values.points_par_pierre_en_plus").get_value<int>();
+    BOOST_FOREACH(ptree::value_type
+                          child, root.get_child("exceeded_values.points_en_plus_en_fonction_des_extremites")) {
+                    conf->exceeded_values.points_en_plus_en_fonction_des_extremites[c] = child.second.get_value<int>();
+                    c++;
+                }
+    conf->points_par_pierre_mangee = root.get_child("points_par_pierre_mangée").get_value<int>();
+}
+
