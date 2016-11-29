a51751b53317fa0bdffa380b0d7bfba5484c48d9
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-11-29 17:02:35 +0100

    clean

--- src/Core/BoardSeeker.cpp
+++ src/Core/BoardSeeker.cpp
@@ -34,36 +34,36 @@ std::list<std::pair<boardPos_t, weight_t>>	*BoardSeeker::getPlayPos(IReferee *re
       tab.push_back(0);
       ++i;
     }
-  //std::cout << "getXPos friend" << std::endl;
-  ref->getBoOp()->getXPossible(2, ref->getPlayer(), &tab, 0);
+  std::cout << "getXPos friend" << std::endl;
+  //ref->getBoOp()->getXPossible(2, ref->getPlayer(), &tab, 0);
   ref->getBoOp()->getXPossible(3, ref->getPlayer(), &tab, 30);
   ref->getBoOp()->getXPossible(4, ref->getPlayer(), &tab, 200);
   ref->getBoOp()->getXPossible(5, ref->getPlayer(), &tab, 1000);
-  //std::cout << "getXPos enemie" << std::endl;
-  ref->getBoOp()->getXPossible(2, !ref->getPlayer(), &tab, 5);
+  std::cout << "getXPos enemie" << std::endl;
+  //ref->getBoOp()->getXPossible(2, !ref->getPlayer(), &tab, 5);
   ref->getBoOp()->getXPossible(3, !ref->getPlayer(), &tab, 25);
   ref->getBoOp()->getXPossible(4, !ref->getPlayer(), &tab, 150);
   ref->getBoOp()->getXPossible(5, !ref->getPlayer(), &tab, 950);
-  //std::cout << "getPosfree amis" << std::endl;
-  ref->getBoOp()->getFreeXPossible(2, ref->getPlayer(), &tab, 20);
+  std::cout << "getPosfree amis" << std::endl;
+  //ref->getBoOp()->getFreeXPossible(2, ref->getPlayer(), &tab, 20);
   ref->getBoOp()->getFreeXPossible(3, ref->getPlayer(), &tab, 300);
   ref->getBoOp()->getFreeXPossible(4, ref->getPlayer(), &tab, 600);
-  //std::cout << "getPosfree enemis" << std::endl;
-  ref->getBoOp()->getFreeXPossible(2, !ref->getPlayer(), &tab, 10);
+  std::cout << "getPosfree enemis" << std::endl;
+  //ref->getBoOp()->getFreeXPossible(2, !ref->getPlayer(), &tab, 10);
   ref->getBoOp()->getFreeXPossible(3, !ref->getPlayer(), &tab, 350);
   ref->getBoOp()->getFreeXPossible(4, !ref->getPlayer(), &tab, 500);
-  //std::cout << "checkeat pos" << std::endl;
+  std::cout << "checkeat pos" << std::endl;
   ref->getBoOp()->getEatPos(ref->getPlayer(), &tab, 400);// savoir combien j'ai mangé
   ref->getBoOp()->getEatPos(!ref->getPlayer(), &tab, 300);// savoir combien j'ai mangé
-  //std::cout << "check doublethree free" << std::endl;
+  std::cout << "check doublethree free" << std::endl;
   ref->getBoOp()->getFreeDoubleThreePos(ref->getPlayer(), &tab, 200);
   ref->getBoOp()->getFreeDoubleThreePos(!ref->getPlayer(), &tab, 199);
-  //std::cout << "check five breakable" << std::endl;
+  std::cout << "check five breakable" << std::endl;
   ref->getBoOp()->getFiveBreakable(ref->getPlayer(), &tab, 1);
   ref->getBoOp()->getFiveBreakable(!ref->getPlayer(), &tab, 500);
   i = XPBOARD + 1;
-  //std::cout << "checkdensity" << std::endl;
-  //std::cout << "addtolist" << std::endl;
+  std::cout << "checkdensity" << std::endl;
+  std::cout << "addtolist" << std::endl;
   //list.reserve(40);
   while (i < PBOARDSIZE - XPBOARD - 1)
     {
@@ -72,15 +72,15 @@ std::list<std::pair<boardPos_t, weight_t>>	*BoardSeeker::getPlayPos(IReferee *re
 	  ref->getBoOp()->getPercentDensityOnPos(i % XPBOARD, i / XPBOARD, &tab, 1);
 	  if (tab[i] > 0)
 	    {
-	      list->push_back(std::pair<boardPos_t, weight_t>(i - XPBOARD - 2 * (i / XPBOARD) + 3, tab[i]));
-	      //std::cout << "WEIGHT pos: " << (i - XPBOARD - 2 * (i / XPBOARD)) + 3 << " = " << tab[i] << std::endl;
+	      list->push_back(std::pair<boardPos_t, weight_t>(i - XPBOARD - 2 * (i / XPBOARD), tab[i]));
+	      std::cout << "WEIGHT pos: " << (i - XPBOARD - 2 * (i / XPBOARD)) << " = " << tab[i] << std::endl;
 	    }
 	}
       ++i;
     }
-  //std::cout << "sort list" << std::endl;
+  std::cout << "sort list" << std::endl;
   list->sort(comparePair);
-  //std::cout << "return" << std::endl;
+  std::cout << "return" << std::endl;
   return (list);
 }
 
