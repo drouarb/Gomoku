96ca3845dfd4f50e1c52cc6857cd0c327964238d
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-11-30 18:51:36 +0100

    improvement in generateReport

--- generateReport.sh
+++ generateReport.sh
@@ -7,14 +7,11 @@ find ./logs/ | sort > .tmp
 mkdir html
 mkdir html/logs
 
-echo "<head>
-    <title>Report</title>
-</head>
-<body>
 
-<h1>All builds</h1>
+FAIL=0
+SUCCESS=0
 
-" > report.html
+function getReport(){
 
 while read file
 do
@@ -24,13 +21,28 @@ do
 
     if [ "$(cat $DEST | grep Error)" = "" ]
     then
-       echo '<img src="'$BLUE'" alt="Succès > Sortie de console" tooltip="Succès > Sortie de console" style="width: 16px; height: 16px; " class="icon-blue icon-sm" title="Succès > Sortie de console">' >> report.html
+       echo '<img src="'$BLUE'" alt="Succès > Sortie de console" tooltip="Succès > Sortie de console" style="width: 16px; height: 16px; " class="icon-blue icon-sm" title="Succès > Sortie de console">'
+           SUCCESS=$(($SUCCESS+1))
     else
-       echo '<img src="'$RED'" alt="Succès > Sortie de console" tooltip="Succès > Sortie de console" style="width: 16px; height: 16px; " class="icon-blue icon-sm" title="Succès > Sortie de console">' >> report.html
+       echo '<img src="'$RED'" alt="Succès > Sortie de console" tooltip="Succès > Sortie de console" style="width: 16px; height: 16px; " class="icon-blue icon-sm" title="Succès > Sortie de console">'
+       FAIL=$(($FAIL+1))
     fi
 
-    echo "<a href=\"$DEST\">$filename</a><br>" >> report.html
+    echo "<a href=\"$DEST\">$filename</a><br>"
 done < .tmp
+}
+
+echo "<head>
+    <title>Report</title>
+</head>
+<body>
+
+<h1>All builds</h1>
+<h2>Success : $SUCCESS, Failures : $FAIL</h2>
+
+" > report.html
+
+getReport >> report.html
 
 echo '</body>' >> report.html
 
